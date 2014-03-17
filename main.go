47db421a447eb0fabb6467f0c918ff1ecad797bb
Jonathan Wills <runningwild@gmail.com>
2014-03-16 21:19:03 -0700

    Got some text showing

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	gl "github.com/chsc/gogl/gl21"
 	"github.com/runningwild/glop/gin"
 	"github.com/runningwild/glop/gos"
+	"github.com/runningwild/glop/gui"
 	"github.com/runningwild/glop/render"
 	"github.com/runningwild/glop/system"
 	"time"
@@ -49,6 +50,17 @@ func mainLoop() {
 		sys.Think()
 		render.Queue(func() {
 			gl.Clear(gl.COLOR_BUFFER_BIT)
+			gl.Disable(gl.DEPTH_TEST)
+			gui.SetFontColor(1, 1, 1, 1)
+			gl.Disable(gl.TEXTURE_2D)
+			gl.MatrixMode(gl.PROJECTION)
+			gl.LoadIdentity()
+			gl.Ortho(gl.Double(0), gl.Double(wdx), gl.Double(wdy), gl.Double(0), 1000, -1000)
+			gl.ClearColor(0, 0, 0, 1)
+			gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+			gl.MatrixMode(gl.MODELVIEW)
+			gl.LoadIdentity()
+			base.GetDictionary("crackin").RenderString("J-Bots", 300, 300, 0, 100, gui.Left)
 		})
 		render.Queue(func() {
 			sys.SwapBuffers()
